16aad7d5d0f9f5ac823d28dab2e0d158c4d4fd38
Shaikh Mahad <codewithmahad+github@gmail.com>
2026-03-04 17:43:29 +0500

    update:application.yaml

--- src/main/java/dev/shaikhmahad/template/SpringbootTemplateApplication.java
+++ src/main/java/dev/shaikhmahad/template/SpringbootTemplateApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootTemplateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootTemplateApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootTemplateApplication.class, args);
+    }
 
 }
